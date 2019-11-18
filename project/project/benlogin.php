54ff071f185341e0057cfae050d1a55cb3819152
Ben <46435167+nickoygwapo123@users.noreply.github.com>
2019-11-18 02:01:15 -0800

    Add files via upload

---
+++ project/project/benlogin.php
@@ -0,0 +1,10 @@
+<?php include('benserver.php')?>
+
+<div class="login">
+	<h1>Login</h1>
+    <form method="post">
+    	<input type="text" name="u" placeholder="Username" required="required" />
+        <input type="password" name="p" placeholder="Password" required="required" />
+        <button type="submit" class="btn btn-primary btn-block btn-large">Click to login.</button>
+    </form>
+</div>
